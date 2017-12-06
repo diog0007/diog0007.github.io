f671b4f6b909870924e42fd51ca51dcd518c7363
Julianna Dioguardi <diog0007@algonquinlive.com>
2017-12-06 15:02:29 -0600

    update gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,8 +2,15 @@
 const gulp = require('gulp')      // load the gulp library
 const sass = require('gulp-sass') // load the gulp-sass compiler library
 const sassPath = 'scss/**/*.scss';
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 // Define a new task called 'sass' that we can call to compile Sass to CSS
 gulp.task('sass', function () {
+  const plugins = [
+    autoprefixer({ browsers: ['last 2 version'] }),
+    cssnano()
+  ]
   return gulp
     .src(sassPath)  // where to find the Sass source files
     .pipe(sass())           // forward those files to the compiler
